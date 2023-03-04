d65634e4aa8d04b389e500a262f844e5e1e06f22
irakli nozadze <iraklinozadze2509@gmail.com>
2023-03-04 20:26:00 +0400

    Fix: group supporting files, add marks

--- PhoneNubersCoreData/ViewController.swift
+++ PhoneNubersCoreData/ViewController.swift
@@ -8,79 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return models.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let model = models[indexPath.row]
-        let cell = tableView.dequeueReusableCell(withIdentifier: "PhoneNumberTableViewCell",
-                                                 for: indexPath) as! PhoneNumberTableViewCell
-        cell.nameLabel.text = model.personsName
-        cell.PhoneNumberLabel.text = model.phoneNumber
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        let item = models[indexPath.row]
-        let sheet = UIAlertController(title: nil,
-                                      message: nil,
-                                      preferredStyle: .actionSheet)
-        sheet.addAction(UIAlertAction(title: "Cancle", style: .cancel, handler: nil))
-        sheet.addAction(UIAlertAction(title: "Edit", style: .default, handler: { _ in
-            let alertController = UIAlertController(title: "Edit",
-                                                    message: "Edit your item",
-                                                    preferredStyle: .alert)
-            alertController.addTextField { (textField : UITextField!) -> Void in
-                textField.placeholder = "Enter Person's New Name"
-            }
-            alertController.addTextField { (textField : UITextField!) -> Void in
-                textField.placeholder = "Enter New Phone Number"
-            }
-            let saveAction = UIAlertAction(title: "Save", style: .cancel, handler: { [weak self] _ in
-                guard let firstTextField = alertController.textFields![0] as UITextField?, let newPersonsName = firstTextField.text,
-                        let secondTextField = alertController.textFields![1] as UITextField?, let newPhoneNumber = secondTextField.text else {
-                    return
-                }
-                
-                if newPersonsName.isEmpty {
-                    self?.updateItemPhoneNumber(item: item, newPhoneNumber: newPhoneNumber)
-                } else if newPhoneNumber.isEmpty {
-                    self?.updateItemName(item: item, newPersonsName: newPersonsName)
-                } else {
-                    self?.updateItem(item: item, newPersonsName: newPersonsName, newPhoneNumber: newPhoneNumber)
-                }
-            })
-            let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertAction.Style.default, handler: { (action : UIAlertAction!) -> Void in })
-                
-            alertController.addAction(saveAction)
-            alertController.addAction(cancelAction)
-
-            self.present(alertController, animated: true, completion: nil)
-        }))
-        sheet.addAction(UIAlertAction(title: "Delete", style: .destructive, handler: { [weak self] _ in
-            self?.deleteItem(item: item)
-        }))
-        
-        self.present(sheet, animated: true)
-    }
-    
-    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-    
-    let tableView: UITableView = {
-        let table = UITableView()
-        table.register(UITableViewCell.self,
-                       forCellReuseIdentifier: "cell")
-        return table
-    }()
-    
+    // MARK: Declaration
     private var models = [PhoneNumbersListItem]()
     
+    
+    // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -179,6 +112,77 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         saveContext()
     }
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    // MARK: TableView
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let model = models[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PhoneNumberTableViewCell",
+                                                 for: indexPath) as! PhoneNumberTableViewCell
+        cell.nameLabel.text = model.personsName
+        cell.PhoneNumberLabel.text = model.phoneNumber
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let item = models[indexPath.row]
+        let sheet = UIAlertController(title: nil,
+                                      message: nil,
+                                      preferredStyle: .actionSheet)
+        sheet.addAction(UIAlertAction(title: "Cancle", style: .cancel, handler: nil))
+        sheet.addAction(UIAlertAction(title: "Edit", style: .default, handler: { _ in
+            let alertController = UIAlertController(title: "Edit",
+                                                    message: "Edit your item",
+                                                    preferredStyle: .alert)
+            alertController.addTextField { (textField : UITextField!) -> Void in
+                textField.placeholder = "Enter Person's New Name"
+            }
+            alertController.addTextField { (textField : UITextField!) -> Void in
+                textField.placeholder = "Enter New Phone Number"
+            }
+            let saveAction = UIAlertAction(title: "Save", style: .cancel, handler: { [weak self] _ in
+                guard let firstTextField = alertController.textFields![0] as UITextField?, let newPersonsName = firstTextField.text,
+                        let secondTextField = alertController.textFields![1] as UITextField?, let newPhoneNumber = secondTextField.text else {
+                    return
+                }
+                
+                if newPersonsName.isEmpty {
+                    self?.updateItemPhoneNumber(item: item, newPhoneNumber: newPhoneNumber)
+                } else if newPhoneNumber.isEmpty {
+                    self?.updateItemName(item: item, newPersonsName: newPersonsName)
+                } else {
+                    self?.updateItem(item: item, newPersonsName: newPersonsName, newPhoneNumber: newPhoneNumber)
+                }
+            })
+            let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertAction.Style.default, handler: { (action : UIAlertAction!) -> Void in })
+                
+            alertController.addAction(saveAction)
+            alertController.addAction(cancelAction)
+
+            self.present(alertController, animated: true, completion: nil)
+        }))
+        sheet.addAction(UIAlertAction(title: "Delete", style: .destructive, handler: { [weak self] _ in
+            self?.deleteItem(item: item)
+        }))
+        
+        self.present(sheet, animated: true)
+    }
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    let tableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self,
+                       forCellReuseIdentifier: "cell")
+        return table
+    }()
 
 }
 
